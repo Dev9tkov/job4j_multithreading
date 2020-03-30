3ec5bcc2b9f4b109aa4f73bb1dd9c6d15f7d0eb8
Dev9tkov <idev9tkov@gmail.com>
2020-03-30 13:58:31 +0500

    3. Прерывание нити

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,36 @@
+package ru.job4j.concurrent;
+
+/**
+ * @author Ilya Devyatkov
+ * @since 30.03.2020
+ */
+public class ConsoleProgress implements Runnable {
+    private String sphereUp = "- / | \\ -";
+    private String sphereDown ="- \\ | / -";
+    private String check = "ne null";
+
+    @Override
+    public void run() {
+            try {
+                while (!Thread.currentThread().isInterrupted()) {
+                    if (check.equals("- / | \\ -")) {
+                        System.out.print("\r load: " + sphereUp);
+                        check = sphereDown;
+                    } else {
+                        System.out.print("\r load: " + sphereDown);
+                        check = sphereUp;
+                    }
+                    Thread.sleep(500);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    public static void main(String[] args) throws InterruptedException{
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000);
+        progress.interrupt();
+    }
+}
