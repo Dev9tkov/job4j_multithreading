b15d2fc6a455e7377f7cae3228581fead087e39d
Dev9tkov <idev9tkov@gmail.com>
2020-04-06 14:26:21 +0500

    0. Блокирование выполнения по условию счетчика[#259654]

---
+++ src/main/java/ru/job4j/wait/Barrier.java
@@ -0,0 +1,38 @@
+package ru.job4j.wait;
+
+/**
+ * @author Ilya Devyatkov
+ * @since 06.04.2020
+ */
+public class Barrier {
+
+    private boolean flag = false;
+
+    private final Object monitor = this;
+
+    public void on() {
+        synchronized (monitor) {
+            flag = true;
+            monitor.notifyAll();
+        }
+    }
+
+    public void off() {
+        synchronized (monitor) {
+            flag = false;
+            monitor.notifyAll();
+        }
+    }
+
+    public void check() {
+        synchronized (monitor) {
+            while (!flag) {
+                try {
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/wait/CountBarrier.java
@@ -0,0 +1,56 @@
+package ru.job4j.wait;
+
+/**
+ * @author Ilya Devyatkov
+ * @since 06.04.2020
+ */
+public class CountBarrier {
+    private final Object monitor = this;
+
+    private final int total;
+
+    private int count = 0;
+
+    public CountBarrier(final int total) {
+        this.total = total;
+    }
+
+    public void count() {
+        synchronized (monitor) {
+            while (count!=total) {
+                count++;
+                monitor.notifyAll();
+            }
+        }
+    }
+
+    public void await() {
+        synchronized (monitor) {
+             while (count != total) {
+                 try {
+                     monitor.wait();
+                 } catch (InterruptedException e) {
+                     Thread.currentThread().interrupt();
+                 }
+             }
+        }
+    }
+
+    public static void main(String[] args) {
+        CountBarrier countBarrier = new CountBarrier(4);
+        Thread first = new Thread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName() + " started");
+                    countBarrier.count();
+                }, "first"
+        );
+        Thread second = new Thread(
+                () -> {
+                    countBarrier.await();
+                    System.out.println(Thread.currentThread().getName() + " started");
+                }, "second"
+        );
+        first.start();
+        second.start();
+    }
+}
