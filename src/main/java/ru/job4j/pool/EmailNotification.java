7c012b0c48d82dd063135db77b54f099a8bad4df
Dev9tkov <idev9tkov@gmail.com>
2020-04-11 16:27:17 +0500

    2. ExecutorService рассылка почты

---
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -0,0 +1,53 @@
+package ru.job4j.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author Ilya Devyatkov
+ * @since 11.04.2020
+ */
+public class EmailNotification {
+    private User user;
+    private ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    public void emailTo(User user) {
+        pool.submit(new Runnable() {
+            @Override
+            public void run() {
+                send(subject(user), body(user), user.getEmail());
+            }
+        });
+    }
+
+    public void close() {
+        pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void send(String subject, String body, String email) {
+
+    }
+
+    private String body (User user) {
+        return new StringBuilder()
+                .append("Add a new event to ")
+                .append(user.getUserName())
+                .toString();
+    }
+
+    private String subject (User user) {
+        return new StringBuilder()
+                .append("Notification ")
+                .append(user.getUserName())
+                .append(" to email ")
+                .append(user.getEmail())
+                .toString();
+    }
+}
---
+++ src/main/java/ru/job4j/pool/User.java
@@ -0,0 +1,32 @@
+package ru.job4j.pool;
+
+/**
+ * @author Ilya Devyatkov
+ * @since 11.04.2020
+ */
+public class User {
+    private String userName;
+    private String email;
+
+    public User(String userName, String email) {
+        this.userName = userName;
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "userName='"
+                + userName + '\''
+                + ", email='" + email + '\''
+                + '}';
+    }
+}
